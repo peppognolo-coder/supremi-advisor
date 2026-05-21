89472db649eb8546db69d51b44f977e885e487d6
peppognolo-coder <peppognolo@gmail.com>
2026-05-21 13:20:48 +0200

    Updated StazioneCard.tsx

--- src/components/StazioneCard.tsx
+++ src/components/StazioneCard.tsx
@@ -39,6 +39,8 @@ export default function StazioneCard({
   isNearest = false,
 }: StazioneCardProps) {
 
+  console.log('STAZIONECARD VERSIONE TEST');
+  
   const [expanded, setExpanded] =
     useState(false);
 
