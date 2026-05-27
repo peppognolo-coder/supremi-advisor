ce8242ccf00ef95c48feb7f54498de5449aa4a99
peppognolo-coder <peppognolo@gmail.com>
2026-05-27 17:55:49 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Disinstalla service worker vecchi
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((registrations) => {
+      for (const registration of registrations) {
+        registration.unregister();
+      }
+    });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
